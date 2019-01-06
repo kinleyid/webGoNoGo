645f18b7f5117a19960135af3b2623e12e9c3c88
Isaac Kinley <Isaac Kinley>
2019-01-06 11:58:34 -0500

    Added gamification

--- run.js
+++ run.js
@@ -10,14 +10,29 @@ var masterBlockwiseLengths = [20, 20, 20];
 var masterBlockwisePpnGos = [0.8, 0.2, 0.5];
 var masterIsGo = generateStimuli(masterBlockwiseLengths, masterBlockwisePpnGos)
 
+var gamify = false;
+if (gamify) {
+	var maxPoints = 50, pointLoss = -maxPoints, currPoints;
+	var pointsBarTimeIncr = 1000/60; // Roughly screen rate
+	var addPointsTimeIncr = 1000/60; // Roughly screen rate
+	var postFeedbackMs = 500;
+    var pointsBarHolder = document.getElementById("pointsBarHolder");
+    var pointsBar = document.getElementById("pointsBar");
+    var pointsBarStopId;
+    var score = 0;
+    scoreArea.style.display = 'block';
+} else {
+    scoreArea.style.display = 'none';
+}
+
 var isPractice = true;
-if(isPractice) {
+if (isPractice) {
 	var practiceBlockwiseLengths = [20];
 	var practiceBlockwisePpnGos = [0.8];
 	var practiceIsGo = generateStimuli(practiceBlockwiseLengths, practiceBlockwisePpnGos)
 }
 
-var trialIdx, wasResponse = false, presentationTime = 0, responseTime = 0;
+var timeoutID, trialIdx, wasResponse = false, presentationTime = 0, responseTime = 0;
 
 var ALL = document.getElementsByTagName("html")[0];
 var dialogArea = document.getElementById("dialogArea");
@@ -30,7 +45,7 @@ allowResponses = false;
 outputText = 'Trial,IsGo,PresentationTime,ResponseTime\n';
 
 function start() {
-	if(isPractice) {
+	if (isPractice) {
 		isGo = practiceIsGo;
 	} else {
 		isGo = masterIsGo;
@@ -48,7 +63,7 @@ function runTrial() {
 	setTimeout(function() {		
 		fixationCross();
 		setTimeout(function() {
-			if(postFixationMs > 0) {
+			if (postFixationMs > 0) {
 				textArea.textContent = '';
 				setTimeout(showStim, postFixationMs);
 			} else {
@@ -63,19 +78,36 @@ function fixationCross() {
 }
 
 function showStim() {
-	if(isGo[trialIdx]) {
+	if (isGo[trialIdx]) {
 		textArea.textContent = "M";
 	} else {
 		textArea.textContent = "W";
 	}
 	allowResponses = true;
-	presentationTime = performance.now();
 	timeoutID = setTimeout(endTrial, allowResponsesMs);
+	if (gamify) {
+		currPoints = maxPoints;
+		pointsBarStopId = setTimeout(function() {
+			pointsBar.style.display = 'block';
+			showPointsBar();
+		}, pointsBarTimeIncr);
+	}
+	presentationTime = performance.now();
+}
+
+function showPointsBar() {
+    if (currPoints >= 0) {
+        pointsBar.style.backgroundColor = "rgb(" + 255*(maxPoints-currPoints)/maxPoints + "," + 255*currPoints/maxPoints + ",0)";
+        pointsBar.style.height = currPoints/maxPoints*pointsBarHolder.clientHeight + 'px';
+        pointsBar.style.top = (1 - currPoints/maxPoints)*pointsBarHolder.clientHeight + 'px';
+        currPoints--;
+        pointsBarStopId = setTimeout(showPointsBar,pointsBarTimeIncr);
+    }
 }
 
 function respondToInput(event) {
 	responseTime = performance.now();
-	if(allowResponses && event.code == 'Space') {
+	if (allowResponses && event.code == 'Space') {
 		clearTimeout(timeoutID);
 		endTrial();
 	}
@@ -89,9 +121,49 @@ function endTrial() {
 		isGo[trialIdx] + ',' +
 		presentationTime + ',' +
 		responseTime + '\n';
+	
+	if (gamify) {
+        pointsBar.style.display = 'none';
+        clearTimeout(pointsBarStopId);
+		if (!isGo[trialIdx]) {
+			currPoints = pointLoss;
+		}
+		addPoints(nextTrial);
+    } else {
+		nextTrial();
+	}
+}
+
+function addPoints(nextFunction) {
+    if (currPoints != 0) {
+		incr = Math.sign(currPoints);
+		if (incr < 0) {
+			scoreArea.style.color = 'red';
+		} else {
+			scoreArea.style.color = 'green';
+		}
+        currPoints -= incr;
+		score += incr;
+		score = Math.max(0, score); // Don't allow negative scores
+        scoreArea.textContent = "Score: " + score;
+        setTimeout(
+            function() {
+                addPoints(nextFunction)
+            },
+            addPointsTimeIncr
+        );
+    } else {
+        setTimeout(function() {
+			scoreArea.style.color = 'black';
+			nextFunction();
+		}, postFeedbackMs);
+    }
+}
+
+function nextTrial() {
 	trialIdx++;
-	if(trialIdx == isGo.length) {
-		if(isPractice) {
+	if (trialIdx == isGo.length) {
+		if (isPractice) {
 			postPracticeScreen();
 		} else {
 			saveData();
@@ -117,7 +189,7 @@ function saveData() {
             textArea.style.display = 'none';
             dialogArea.style.display = 'block';
             dialogArea.textContent = 'Thank you!';
-        } else if(xhttp.status == 500) {
+        } else if (xhttp.status == 500) {
             saveData();
         }
     };
@@ -129,7 +201,7 @@ function saveData() {
 
 function generateStimuli(lens, ppnGos) {
 	var allBlocks = new Array();
-	for(i = 0; i < lens.length; i++) {
+	for (i = 0; i < lens.length; i++) {
 		currBlock = Array(Math.round(lens[i]*ppnGos[i])).fill(true).concat(
 			Array(Math.round(lens[i]*(1 - ppnGos[i]))).fill(false));
 		allBlocks = allBlocks.concat(sample(currBlock, currBlock.length));
@@ -141,11 +213,11 @@ function sample(urInArray, nSamples) {
 	// If nSamples > 1, return array, else return single element
 	var inArray = urInArray.slice(0); // Don't alter original array
 	var outArray = [], i, idx;
-	for(i = 0; i < nSamples; i++) {
+	for (i = 0; i < nSamples; i++) {
 		idx = Math.floor(Math.random()*inArray.length);
 		outArray.push(inArray.splice(idx, 1)[0]);
 	}
-	if(nSamples > 1) {
+	if (nSamples > 1) {
 		return outArray;
 	} else {
 		return outArray[0];
